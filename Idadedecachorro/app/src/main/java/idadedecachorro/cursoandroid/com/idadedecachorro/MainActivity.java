ae4936e3b26933022024395b6e26b77b06087d3b
vittorfl <vittorfl@gmail.com>
2017-02-26 23:53:56 -0300

    Frase do dia e Idade do cachorro

---
+++ Idadedecachorro/app/src/main/java/idadedecachorro/cursoandroid/com/idadedecachorro/MainActivity.java
@@ -0,0 +1,32 @@
+package idadedecachorro.cursoandroid.com.idadedecachorro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText caixaTexto;
+    private Button botaoIdade;
+    private TextView resultadoIdade;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        caixaTexto = (EditText) findViewById(R.id.caixaTextoId);
+        botaoIdade = (Button) findViewById(R.id.botaoIdadeId);
+        resultadoIdade = (TextView) findViewById(R.id.resultadoIdadeId);
+
+        botaoIdade.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+}
